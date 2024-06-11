ed5d9bb97c86ecc233a2d1e0a008d8d9e5b63068
KasMichta <kacper.michta33@gmail.com>
2024-06-11 15:49:14 +0100

    Add help for function

--- SeededGuid/Public/Get-SeededGuid.ps1
+++ SeededGuid/Public/Get-SeededGuid.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS 
+    Generates a GUID from a seed string.
+.DESCRIPTION
+    This function generates a GUID from a seed string. The GUID is deterministic, meaning that the same seed will always produce the same GUID.
+
+.PARAMETER Seed
+    The seed string to generate the GUID from.
+
+.INPUTS
+    System.String
+
+.OUTPUTS
+    System.Guid
+
+.EXAMPLE
+    PS> Get-SeededGuid -Seed 'Hello, World!'
+    
+    Guid
+    ----
+    98500190-d23c-b04f-d696-3f7d28e17f72
+
+#>
 Function Get-SeededGuid {
     [CmdletBinding()]
     [OutputType([guid])]
